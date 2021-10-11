df03bd4a68e09d39dfdf28cf44d029905cbee52f
sTvynator <83620243+sTvynator@users.noreply.github.com>
2021-10-11 10:53:13 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,18 +9,7 @@ import config
 client = discord.Client()
 
 
-async def update_player_count():
-    while not client.is_closed:
-        await client.wait_until_ready()
-        try:
-            with valve.source.a2s.ServerQuerier(config.SERVER_ADDRESS) as server:
-                await client.change_presence(
-                    game=discord.Game(
-                        name=f"{server.info()['player_count']} players online!"))
-        except valve.source.NoResponseError:
-            pass
 
-        await asyncio.sleep(3)
 
 
 client.loop.create_task(update_player_count())
